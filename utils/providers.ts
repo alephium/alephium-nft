4267fd2a44ce7c0679fa35fa6d415108add83728
Hongchao Liu <lbs.lhc@gmail.com>
2022-06-14 00:55:30 +0200

    Minimal change to reproduce the wallet connect deps issue

--- utils/providers.ts
+++ utils/providers.ts
@@ -1,6 +1,22 @@
 import { NodeProvider } from 'alephium-web3'
+import WalletConnectClient from '@walletconnect/client'
 
-const localProvider = new NodeProvider('http://127.0.0.1:22973')
+const localDevProvider = new NodeProvider('http://127.0.0.1:22973')
 // Other providers
 
-export const provider = localProvider
+export const provider = localDevProvider
+
+export async function walletConnectCallback() {
+  const walletConnect = await WalletConnectClient.init({
+    projectId: '6e2562e43678dd68a9070a62b6d52207',
+    relayUrl: 'wss://relay.walletconnect.com',
+    metadata: {
+      name: 'Alphium NFT',
+      description: 'Alpephium NFT Marketplace',
+      url: 'https://walletconnect.com/',
+      icons: ['https://walletconnect.com/walletconnect-logo.png']
+    }
+  })
+
+  console.log(walletConnect)
+}
