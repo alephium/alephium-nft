73f950d6bd5d03d9ee91c612bc6b691afebca0b6
h0ngcha0 <lbs.lhc@gmail.com>
2023-01-11 06:32:37 +0100

    Update testnet addresses

--- configs/addresses.ts
+++ configs/addresses.ts
@@ -4,12 +4,12 @@ export const defaultNftCollectionContractId: string =
   CLUSTER === 'mainnet'
   ? ''
   : CLUSTER === 'testnet'
-  ? '9b9a51faedb5e33d870ff7e2dc382912213cfe325ce2e58ab882989f629857b3'
+  ? '5348fc12b920f4052040049098248ac67c03f6f0e6a4f376be1558cc8e510a23'
   : '165330c18dc1e5c71ec2fa29c6cebdf46fc2a175259382f187248ffbfa051000'
 
 export const marketplaceContractId: string =
   CLUSTER === 'mainnet'
   ? ''
   : CLUSTER === 'testnet'
-  ? '6bd062326ed0958cecca2a93f1d452b8745dc2cf52cf1d2d3fc5aff02a703d2a'
+  ? 'f6344f27797b8fcd5c5a4cee1b3458bb7099fb0e8293336f5506e59d6603c185'
   : 'a8a6359f6c37fb2986d8bf95d1b7ca71365281d83247e740cdc6cffd0b6b0700'
