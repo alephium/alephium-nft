0f00d810fbcad685d5e44d2a58a706af8e80e7ab
h0ngcha0 <lbs.lhc@gmail.com>
2023-06-02 17:47:57 +0200

    Collection details page

--- pages/collection-details.tsx
+++ pages/collection-details.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from 'next/router'
 import { useAlephiumConnectContext } from '@alephium/web3-react'
-import Link from 'next/link'
 import { useCollection } from '../components/nft-collection'
+import { Button, NFTCard } from '../components'
+import Image from 'next/image';
+import images from '../assets';
+import { shortenAddress } from '../utils/shortenAddress';
 
-export default function Collections() {
+export default function CollectionDetails() {
   const context = useAlephiumConnectContext()
   const router = useRouter()
   const { collectionId } = router.query
@@ -16,54 +19,66 @@ export default function Collections() {
     <>
       {
         collection && (
-          <>
-            <div className="flex justify-center">
-              <table className="w-1/2 flex flex-col pb-12">
-                <tbody>
-                  <tr>
-                    <td>
-                      <img className="rounded mt-4" src={collection.image} />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="whitespace-nowrap text-sm font-medium"><b>Collection Name</b>: {collection.name}</td>
-                  </tr>
-                  <tr>
-                    <td className="whitespace-nowrap text-sm font-medium"><b>Description</b>: {collection.description}</td>
-                  </tr>
-                  <tr>
-                    <td className="whitespace-nowrap text-sm font-medium"><b>Total Supply</b>: {collection.totalSupply.toString()}</td>
-                  </tr>
-                  <tr>
-                    <td className="whitespace-nowrap text-sm font-medium">
-                      <b>Already Minted</b>: {collection.totalSupply.toString().concat(',  ')}
-                      <Link href={`/mint-nfts?collectionId=${collection.id}`}><a className="mr-6 text-blue-500">mint more </a></Link>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+          <div className="relative flex justify-center md:flex-col min-h-screen">
+            <div className="relative flex-1 flexCenter sm:px-4 p-12 border-r md:border-r-0 md:border-b dark:border-nft-black-1 border-nft-gray-1">
+              <div className="relative w-557 minmd:w-2/3 minmd:h-2/3 sm:w-full sm:h-300 h-557 ">
+                <Image src={collection.image} objectFit="cover" className=" rounded-xl shadow-lg" layout="fill" />
+              </div>
             </div>
+            <div className="flex-1 justify-start sm:px-4 p-12 sm:pb-4">
+              <div className="flex flex-row sm:flex-col">
+                <h2 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl minlg:text-3xl">{collection.name}</h2>
+              </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-              {
-                collection.nfts.map((nft, i) => {
-                  return (
-                    <div key={i} className="border shadow rounded-xl overflow-hidden">
-                      <div className="p-4 object-center">
-                        <img src={nft.image} />
-                      </div>
-                      <div className="p-4">
-                        <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
-                        <div style={{ height: '70px', overflow: 'hidden' }}>
-                          <p className="text-gray-400">{nft.description}</p>
-                        </div>
-                      </div>
-                    </div>
-                  )
-                })
-              }
+              <div className="mt-10">
+                <p className="font-poppins dark:text-white text-nft-black-1 text-xs minlg:text-base font-normal">Owner</p>
+                <div className="flex flex-row items-center mt-3">
+                  <div className="relative w-12 h-12 minlg:w-20 minlg:h-20 mr-2">
+                    <Image src={images.creator1} objectFit="cover" className="rounded-full" />
+                  </div>
+                  <p className="font-poppins dark:text-white text-nft-black-1 text-sm minlg:text-lg font-semibold">
+                    {shortenAddress(context.account?.address)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-10 flex flex-col">
+                <div className="w-full border-b dark:border-nft-black-1 border-nft-gray-1 flex flex-row">
+                  <p className="font-poppins dark:text-white text-nft-black-1 font-medium text-base mb-2">Details</p>
+                </div>
+                <div className="mt-3">
+                  <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-base">
+                    {collection.description}
+                  </p>
+                </div>
+              </div>
+              <div className="mt-10 flex flex-wrap">
+                <div className="w-full border-b dark:border-nft-black-1 border-nft-gray-1 flex flex-row">
+                  <p className="font-poppins dark:text-white text-nft-black-1 font-medium text-base mb-2">
+                    {`${collection.totalSupply} NFTs`}
+                  </p>
+                </div>
+                {
+                  collection.nfts.map((nft, i) => {
+                    return (
+                      <NFTCard
+                        key={i}
+                        nft={{ tokenOwner: context.account?.address || '', ...nft }}
+                      />
+                    )
+                  })
+                }
+              </div>
+
+              <div className="flex flex-row sm:flex-col mt-10">
+                <Button
+                  btnName={"Mint More"}
+                  classStyles="mr-5 sm:mr-0 sm:mb-5 rounded-xl"
+                  handleClick={() => router.push(`/mint-nfts?collectionId=${collection.id}`)}
+                />
+              </div>
             </div>
-          </>
+          </div>
         )
       }
     </>
