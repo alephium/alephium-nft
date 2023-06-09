875c68a2a828ac0c1cf9213f53c232d606898c62
h0ngcha0 <lbs.lhc@gmail.com>
2023-06-09 05:25:02 +0200

    Fix collection & nft image position in details page

--- pages/nft-details.tsx
+++ pages/nft-details.tsx
@@ -149,7 +149,7 @@ const AssetDetails = () => {
 
   return (
     <div className="relative flex justify-center md:flex-col min-h-screen">
-      <div className="relative flex-1 flexCenter sm:px-4 p-12 border-r md:border-r-0 md:border-b dark:border-nft-black-1 border-nft-gray-1">
+      <div className="relative flex-1 flexTop sm:px-4 p-12 border-r md:border-r-0 md:border-b dark:border-nft-black-1 border-nft-gray-1">
         <div className="relative w-557 minmd:w-2/3 minmd:h-2/3 sm:w-full sm:h-300 h-557 ">
           <Image src={nft.image} objectFit="cover" className=" rounded-xl shadow-lg" layout="fill" />
         </div>
