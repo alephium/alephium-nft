e78ba10a86bc4f5dfaf6b571ecd0fcee589a74e2
lbqds <lbqds@outlook.com>
2023-07-25 19:50:24 +0800

    Support loading old minted nfts

--- utils/nft.ts
+++ utils/nft.ts
@@ -1,6 +1,9 @@
-import { addressFromTokenId, groupOfAddress, hexToString, web3 } from "@alephium/web3"
+import { addressFromContractId, addressFromTokenId, groupOfAddress, hexToString, node, web3 } from "@alephium/web3"
 import axios from "axios"
 import { NFT, NFTTypes } from "../artifacts/ts"
+
+const LegacyEnumerableNFTCodeHash = '9754329f7623044ec5edeb7a3349eb36066ec1140fb41e2ba5828685df1e1012'
+const LegacyNonEnumerableNFTCodeHash = '657b46b2236b78c955660db6731d43b42dd8204bb6220181de3c5f8678d90da0'
 
 export interface NFT {
   name: string,
@@ -25,6 +28,24 @@ export async function fetchMintedNFTMetadata(tokenId: string): Promise<{ collect
         tokenUri: hexToString(contractState.fields.tokenUri),
         collectionId: contractState.fields.collectionId
       }
+    } else if (nftState.codeHash === LegacyNonEnumerableNFTCodeHash) {
+      return {
+        collectionId: nftState.immFields[0].value as string,
+        tokenUri: hexToString(nftState.immFields[1].value as string)
+      }
+    } else if (nftState.codeHash === LegacyEnumerableNFTCodeHash) {
+      const collectionId = nftState.immFields[0].value as string
+      const nftIndex = nftState.immFields[1]
+      const address = addressFromContractId(collectionId)
+      const call: node.CallContract = {
+        group: groupOfAddress(address),
+        address: address,
+        methodIndex: 4,
+        args: [nftIndex]
+      }
+      const callResult = await nodeProvider.contracts.postContractsCallContract(call)
+      const tokenUri = hexToString(callResult.returns[0].value as string)
+      return { tokenUri, collectionId }
     }
   } catch (error) {
     console.error(`failed to fetch nft metadata, token id: ${tokenId}, error: ${error}`)
